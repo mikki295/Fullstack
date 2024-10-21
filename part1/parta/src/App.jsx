3abaf141648e5813eafb2348d7da94a6af3a353b
mikki295 <mikael.demeulder@hotmail.com>
2024-10-21 13:24:09 +0300

    Kurssitiedot1 and Kurssitiedo2 done

---
+++ part1/parta/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  console.log('App käynnistyy')
+  const friends = ['Leevi','Kalevi']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+
+export default App
