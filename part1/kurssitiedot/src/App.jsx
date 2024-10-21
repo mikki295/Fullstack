37138b96a1440fc1d12f5d64c5910ad8ac495458
mikki295 <mikael.demeulder@hotmail.com>
2024-10-21 14:32:12 +0300

    Exercises 1.3 - 1.5 done

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -7,19 +7,22 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  const [first, second, third] = props.parts
+
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={first.name} exercises={first.exercises} />
+      <Part part={second.name} exercises={second.exercises} />
+      <Part part={third.name} exercises={third.exercises} />
     </div>
   )
 }
 
 const Total = (props) => {
+  const [first, second, third] = props.parts
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {first.exercises + second.exercises + third.exercises}</p>
     </div>
   )
 }
@@ -36,19 +39,29 @@ const Part = (props) => {
 
 
 const App = () => {
-  const course      = 'Half Stack application development'
-  const part1       = 'Fundamentals of React'
-  const exercises1  = 10
-  const part2       = 'Using props to pass data'
-  const exercises2  = 7
-  const part3       = 'State of a component'
-  const exercises3  = 14
+  const course = {
+    name: 'Half Stack Application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-        <Header course={course} />
-        <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
---
+++ part1/partb/test.js
@@ -0,0 +1,10 @@
+function product(a,b) {
+    return a*b
+}
+
+const average = function(a,b) {
+    return (a+b) / 2
+}
+
+const result = average(2,6)
+console.log(result)
